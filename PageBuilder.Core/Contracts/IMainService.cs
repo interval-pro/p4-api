e089239f43d49ab8b21a0b75c0245fa29095a43e
Georgi M. Kuzmanov <47320878+gmkuzmanoff@users.noreply.github.com>
2024-06-13 16:27:27 +0300

    Update Main Service interface

--- PageBuilder.Core/Contracts/IMainService.cs
+++ PageBuilder.Core/Contracts/IMainService.cs
@@ -4,7 +4,11 @@ namespace PageBuilder.Core.Contracts
 {
     public interface IMainService
     {
+        Task<string> GenerateImageAsync(CreatePageModel jsonRequest);
+        
         Task<string> GeneratePageAsync(CreatePageModel jsonRequest);
+
+        Task<string> ImageColorExtractAsync(CreatePageModel jsonRequest);
 
         Task<string> UpdatePageAsync(UpdatePageModel updateData, CreatePageModel currentData);
     }
