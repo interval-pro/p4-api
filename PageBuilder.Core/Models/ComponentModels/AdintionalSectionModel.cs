05806d8298944bd154ea7c78ec74c88cde0dd3f1
H.Vasev <hristovasev999@gmail.com>
2024-06-16 14:05:05 +0100

    Add multiple classes to model layout and section data

---
+++ PageBuilder.Core/Models/ComponentModels/AdintionalSectionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PageBuilder.Core.Models.ComponentModels
+{
+    public class AdintionalSectionModel
+    {
+        public string? InitialInputs { get; set; }
+
+        public string SectionId { get; set; } = string.Empty;
+
+        public List<ComponentModel> Components { get; set; } = new List<ComponentModel>();
+    }
+}
